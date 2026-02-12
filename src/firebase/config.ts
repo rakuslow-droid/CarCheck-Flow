e22567ea8888e4e7d5f990da4c62c46926491c18
渡辺友樹 <raku.slow@gmail.com>
2026-02-12 09:34:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2121462860-3a10a",
+  "appId": "1:1013482335269:web:998a2c2c759a38a59b23cc",
+  "apiKey": "AIzaSyByejwfVt42nBluPA0EPrgpjhv5MVXDvxg",
+  "authDomain": "studio-2121462860-3a10a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1013482335269"
+};
